0efd00548d758b662a5ae16beae2e70b899a6525
Samira Abdoulkarim <72474158+Sasso17@users.noreply.github.com>
2025-01-27 12:12:09 +0100

    initialisation du projet Chatop

---
+++ src/test/java/com/backEndJavaSpring/Chatop_app/ChatopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backEndJavaSpring.Chatop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
